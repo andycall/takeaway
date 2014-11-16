14396c0852007307fdfe543015ea91d7c2a97e50
董小姐 <dongtiangche@gmail.com>
2014-11-16 23:42:41 +0800

    merged

--- app/views/tools/Sizer.js
+++ app/views/tools/Sizer.js
@@ -1,11 +1,26 @@
 define(['jquery'], function($){
+	function isInArray(value, arr){
+		for(var i = 0,len = arr.length; i < len ; i++){
+			if(arr[i] === value){
+				return i;
+			}
+		}
+		return -1;
+	}
+
 	function Sizer(){
 		this.data = {};
 	}
 
 
-	// 添加队列
-	Sizer.prototype.add = function(value, label){
+	/**
+	 *
+	 * @param
+	 * group 数据的组别
+	 *
+	 *
+	 */
+	Sizer.prototype.add = function(label, value){
 		var self = this;
 
 		if(!self.data[label]){
@@ -13,9 +28,39 @@ define(['jquery'], function($){
 		}
 		if($.isFunction(value)) return;
 
+		if(!$.isArray(value)) value = [value];
+
 		self.data[label].push(value);
+	};
+
+
+	// 获取
+	Sizer.prototype.get = function(labels){
+		var self = this,
+			data = self.data,
+			index;
+
+		if(!$.isArray(labels)) labels = [labels];
+
+		var result = [];
+
+		for(var key in data){
+			index = isInArray(key, labels);
+
+			if(index >= 0){
+				result.push({
+					label : labels[index],
+					value : data[key]
+				});
+				labels.pop(index);
+			}
+		}
+
+		return result;
+	};
+
+
+	return {
+		Sizer : new Sizer()
 	}
-
-	
-
 });
--- public/js/lib/Sizer.js
+++ public/js/lib/Sizer.js
@@ -1,3 +1,21 @@
-/**
- * Created by andycall on 14-11-16.
- */
+define(['jquery'], function($){
+	function Sizer(){
+		this.data = {};
+	}
+
+
+	// 添加队列
+	Sizer.prototype.add = function(value, label){
+		var self = this;
+
+		if(!self.data[label]){
+			self.data[label] = [];
+		}
+		if($.isFunction(value)) return;
+
+		self.data[label].push(value);
+	}
+
+	
+
+});
