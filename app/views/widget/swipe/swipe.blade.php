d06fd0ec23420ca1af68c5d7df5ad38d9fd00503
Liuchenling <wssgcg1213@qq.com>
2014-11-16 11:56:40 +0800

    结构乱

--- app/views/widget/swipe/swipe.blade.php
+++ app/views/widget/swipe/swipe.blade.php
@@ -1,23 +1,32 @@
 <div class="swipe">
-	<div class="swipe-out">
-		<div class="in">
-            <img src="##"/>
-            <img src="##"/>
-            <img src="##"/>
-            <img src="##"/>
-            <img src="##"/>
-            <img src="##"/>
-            <img src="##"/>
-		</div>
-	</div>
-	<div class="swipe-nav">
-	    <ul>
-	        <li class="selected">1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li>
-	    </ul>
-	</div>
+	<!--<div class="swipe-out">-->
+		<!--<div class="in">-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+            <!--<img src="##"/>-->
+		<!--</div>-->
+	<!--</div>-->
+	<!--<div class="swipe-nav">-->
+	    <!--<ul>-->
+	        <!--<li class="selected">1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li>-->
+	    <!--</ul>-->
+	<!--</div>-->
+    <div id="m-slider" class="slideBox">
+        <ul class="items">
+            <li><a href="http://www.jq22.com/" title="这里是测试标题一"><img src="http://assets.jq22.com/plugin/pc-9e41e590-2db4-11e4-954d-000c29f61318.png"></a></li>
+            <li><a href="http://www.jq22.com/" title="这里是测试标题二"><img src="http://assets.jq22.com/plugin/pc-9e41e590-2db4-11e4-954d-000c29f61318.png"></a></li>
+            <li><a href="http://www.jq22.com/" title="这里是测试标题三"><img src="http://assets.jq22.com/plugin/pc-9e41e590-2db4-11e4-954d-000c29f61318.png"></a></li>
+            <li><a href="http://www.jq22.com/" title="这里是测试标题四"><img src="http://assets.jq22.com/plugin/pc-9e41e590-2db4-11e4-954d-000c29f61318.png"></a></li>
+            <li><a href="http://www.jq22.com/" title="这里是测试标题五"><img src="http://assets.jq22.com/plugin/pc-9e41e590-2db4-11e4-954d-000c29f61318.png"></a></li>
+        </ul>
+    </div>
 </div>
 
 @section("css")
     @parent
-    {{HTML::style("/css/widget/home/swipe/swipe.css")}}
+    {{HTML::style("/css/widget/swipe/swipe.css")}}
 @stop
