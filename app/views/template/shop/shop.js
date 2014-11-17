41a717d8da550794b232c2caf49752a4b9ef79ff
董小姐 <dongtiangche@gmail.com>
2014-11-17 15:52:51 +0800

    JS load complete

--- app/views/template/shop/shop.js
+++ app/views/template/shop/shop.js
@@ -8,13 +8,27 @@ require.config({
 	paths : {
 		shop : "../template/shop",
 		userBar : "../widget/userBar",
+		cate_category : "../widget/cate_category",
+		cate_list   : "../widget/cate_list",
+		goods_collection : "../widget/goods_collection",
+		hot_sails : "../widget/hot_sails",
+		restaurant_announcement : "../widget/restaurant_announcement",
+		shop_details : "../widget/shop_details",
+		shop_map : "../widget/shop_map"
 
 	}
 });
 
 // 加载项目所需的所有依赖项
 define([
-	'userBar/userBar',
+	"userBar/userBar",
+	"cate_category/cate_category",
+	"cate_list/cate_list",
+	"goods_collection/goods_collection",
+	"hot_sails/hot_sails",
+	"restaurant_announcement/restaurant_announcement",
+	"shop_details/shop_details",
+	"shop_map/shop_map"
 ], function($){
 	console.log("init");
 });
