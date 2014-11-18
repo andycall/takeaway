b4e95d4100de7bf46dbd4aa95e018286d66182ba
董小姐 <dongtiangche@gmail.com>
2014-11-19 01:37:22 +0800

    persona_center

--- app/views/widget/personal_center/personal_center.blade.php
+++ app/views/widget/personal_center/personal_center.blade.php
@@ -1,4 +1,31 @@
-<div>个人中心</div>
+<div class="content_header">
+    <div class="ui-helper-clearfix">
+        <h2>个人中心</h2>
+    </div>
+</div>
+<div class="content_inner">
+    <div class="account_state">
+        <div class="ui-helper-clearfix">
+            <div class="col_left">
+                <div class="avatar">
+
+                </div>
+                <div class="ava_wrapper">
+                    <h5>Lecion</h5>
+                    <p>
+                        <span>安全等级</span
+                        <a class="user-level high">较高</a>
+                    </p>
+                </div>
+            </div>
+            <div class="col_right"></div>
+        </div>
+        <div class="relate_info ui-helper-clearfix">
+
+        </div>
+    </div>
+    <div class="last_order"></div>
+</div>
 
 @section("css")
     @parent
