0cd798bb9510b31a12f3cf5aa1222a3728fcc8c0
董小姐 <dongtiangche@gmail.com>
2014-11-23 02:32:04 +0800

    cate_list not finished

--- app/views/template/shop/shop.blade.php
+++ app/views/template/shop/shop.blade.php
@@ -8,31 +8,36 @@
 @section("shop_details")
     @include("widget/shop_details/shop_details")
 @stop
+
 {{-- 美食分类 --}}
 @section("cate_category")
     @include("widget/cate_category/cate_category")
 @stop
+
 {{-- 美食列表--}}
 @section("cate_list")
     @include("widget/cate_list/cate_list")
 @stop
+
  {{-- 餐厅公告 --}}
 @section("restaurant_announcement")
     @include("widget/restaurant_announcement/restaurant_announcement")
 @stop
+
 {{-- 我的收藏(商品) --}}
 @section("goods_collection")
     @include("widget/goods_collection/goods_collection")
 @stop
+
 {{-- 本周热卖 --}}
 @section("hot_sails")
     @include("widget/hot_sails/hot_sails")
 @stop
+
 {{-- 商店地图 --}}
 @section("shop_map")
     @include("widget/shop_map/shop_map")
 @stop
-
 
 @section("footer")
 	@include("widget.footer.footer")
--- app/views/widget/cate_list/cate_list.blade.php
+++ app/views/widget/cate_list/cate_list.blade.php
@@ -10,9 +10,51 @@
                 <ul class="activity_drop_down"></ul>
             </div>
         </div>
-        <a href="" class="tool_item">评分
-        </a>
+        <a href="" class="tool_item ui_on">默认排序</a>
+        <a href="" class="tool_item">评分<i class="glyph-sort-down"></i></a>
+        <a href="" class="tool_item">销量<i class="glyph-sort-down"></i></a>
+        <a href="" class="tool_item">价格<i class="glyph-sort-down"></i></a>
     </div>
+</div>
+
+<div class="cate_view">
+    <section>
+        <h2 title="套餐类">套餐类</h2>
+        <p  class="ad_sec" title="每天可享受两单优惠，每单可享受3份特价菜。">每天可享受两单优惠，每单可享受3份特价菜。</p>
+        <ul class="menu_list">
+            <li class="menu_list_block">
+                <div class="menu_sec_info">
+                    <p class="menu_sec_title">
+                        <a href="#" class="dish_flavor favor_btn">♥</a>
+                        <a href="#" class="dish_title">意大利披萨</a>
+                    </p>
+                    <p class="menu_sec_desc" title="麻辣李素">麻辣李素</p>
+                </div>
+                <div class="menu_sec_note">
+                    <span class="rst-d-ordered dish_state">2</span>
+                </div>
+                <div class="menu_sec_action">
+                    <div class="dish_act act_btn">
+                        <a href="" class="rst-d-act-add add_btn" title="点击一份">
+                            <span class="rst-d-act-glyph"></span>
+                            <span class="price symbol-rmb">28</span>
+                        </a>
+                        <a href="" class="rst-d-act-toggle caret add_main_btn"></a>
+                        <div style="display: none" class="rst-d-act-dropdown main_food_panel">
+                            <span class="helper">添加到以下菜品中</span>
+                            <span class="dish single_main_food">意大利皮塞</span>
+                            <span class="dish single_main_food">皮蛋炒肉</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="menu_sec_status">
+                    <span class="rst-d-rating food_rating">
+                        <i class="icon-d-star s0 i_s"></i>
+                    </span>
+                </div>
+            </li>
+        </ul>
+    </section>
 </div>
 
 
