39d233e8117c57c34dbe763e485bd276b1a36e38
Liuchenling <wssgcg1213@qq.com>
2014-11-23 19:18:42 +0800

    shop_cart add

--- public/js/widget/shop_cart/shop_cart.js
+++ public/js/widget/shop_cart/shop_cart.js
@@ -1,3 +1,11 @@
-define([ "jquery" ], function() {
-    console.log("shop cart loaded");
+define([ "jquery" ], function($) {
+    function toggleCartScroll() {
+        $cartUp.animate(parseInt($cartUp.css("top")) ? {
+            top: "0px"
+        } : {
+            top: -$cartUp.height() + "px"
+        });
+    }
+    var $cartUp = $("#cartScroll");
+    $(".aside-icon-cart").on("click", toggleCartScroll), console.log("shop cart loaded");
 });
