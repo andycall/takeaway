fbf048a5e4cf578c00f20c721c119caf59ccba68
unknown <492994082@qq.com>
2014-11-27 22:09:11 +0800

    update index.php ajax

--- public/js/widget/my_collection_alert/my_collection_alert.js
+++ public/js/widget/my_collection_alert/my_collection_alert.js
@@ -1,16 +1,21 @@
 define([ "jquery" ], function($) {
-    console.log("my collection alert loaded");
-    var n, cancel_collection = [], add_collection = [];
-    $(".add").on("click", function() {
-        $(".collection-modal").css("display", "block"), $(".collection-modal input").each(function() {
-            $(this).is(":checked") && (n = $(this).attr("shop_id"), cancel_collection[n] = new Array(2), 
-            cancel_collection[n].shop_id = n, cancel_collection[n].place_id = $(this).attr("place_id"));
+    function cancel_collection_each() {
+        $(".collection-modal input").each(function() {
+            this.checked && (n = $(this).attr("shop_id"), cancel_collection["shop_id_" + n] = new Array(2), 
+            cancel_collection["shop_id_" + n].shop_id = n, cancel_collection["shop_id_" + n].place_id = $(this).attr("place_id"));
         });
+    }
+    console.log("my collection alert loaded");
+    var n, cancel_collection = [], add_collection = [], post = {};
+    window.onload = function() {
+        cancel_collection_each();
+    }, $(".add").on("click", function() {
+        $(".collection-modal").css("display", "block");
     }), $(".collection-modal").on("click", function(e) {
         var e_class = e.target.className || null;
         switch (e_class) {
           case "check":
-            var shop_id = $(this).attr("shop_id"), click_checked = $(this)[0].checked, sameElement = $(".collection-modal").find("input[shop_id='" + shop_id + "']");
+            var shop_id = $(e.target).attr("shop_id"), click_checked = e.target.checked, sameElement = $(".collection-modal").find("input[shop_id='" + shop_id + "']");
             sameElement.each(function() {
                 this.checked = click_checked;
             });
@@ -22,12 +27,28 @@ define([ "jquery" ], function($) {
 
           case "btn btn-yellow":
             $(".collection-modal").css("display", "none"), $(".collection-modal input").each(function() {
-                $(this).is(":checked") && (n = $(this).attr("shop_id"), add_collection[n] = new Array(2), 
-                add_collection[n].shop_id = n, add_collection[n].place_id = $(this).attr("place_id"));
-            }), add_collection.forEach(function(value, index) {
-                cancel_collection[index] ? (cancel_collection.splice(index, 1), add_collection.splice(index, 1)) : (add_collection[n] = new Array(2), 
-                add_collection[n].shop_id = n, add_collection[n].place_id = $(this).attr("place_id"));
-            }), cancel_collection = [], add_collection = [];
+                this.checked && (n = $(this).attr("shop_id"), add_collection["shop_id_" + n] = new Array(2), 
+                add_collection["shop_id_" + n].shop_id = n, add_collection["shop_id_" + n].place_id = $(this).attr("place_id"));
+            }), Object.keys(add_collection).forEach(function(value) {
+                cancel_collection[value] && (delete cancel_collection[value], delete add_collection[value]);
+            }), post.add_collection = [], post.cancel_collection = [], Object.keys(add_collection).forEach(function(value, index) {
+                post.add_collection[index] = {}, post.add_collection[index] = {
+                    shop_id: add_collection[value].shop_id,
+                    place_id: add_collection[value].place_id
+                };
+            }), Object.keys(cancel_collection).forEach(function(value, index) {
+                post.cancel_collection[index] = {}, post.cancel_collection[index] = {
+                    shop_id: cancel_collection[value].shop_id,
+                    place_id: cancel_collection[value].place_id
+                };
+            }), (post.add_collection.length || post.cancel_collection.length) && $.ajax({
+                url: "takeaway/public/index.php/collect-list",
+                type: "POST",
+                data: post,
+                success: function(res) {
+                    "true" == res.success || alert("收藏失败，请重新收藏");
+                }
+            }), cancel_collection = [], cancel_collection_each(), add_collection = [];
             break;
 
           case "p_hot":
