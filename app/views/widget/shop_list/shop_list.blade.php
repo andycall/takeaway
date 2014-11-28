546252a8ee95d01e327fb525152c727866ea6fd5
董小姐 <dongtiangche@gmail.com>
2014-11-29 02:05:21 +0800

    main delay data

--- app/views/widget/shop_list/shop_list.blade.php
+++ app/views/widget/shop_list/shop_list.blade.php
@@ -34,9 +34,9 @@
         <div class="shop_activities">
             <div class="ui-helper-clearfix">
             @foreach($shop_list['data']['activity'] as $key=>$value)
-                    <span class="activities-btn" data-activity-id="{{$key}}">
-                       <input id="checkbox_btn_{{$key}}" class="sh_hot_shops" name="filter" type="checkbox">
-                       <label for="checkbox_btn_{{$key}}">{{$value}}</label>
+                    <span class="activities-btn" data-activity_id="{{$key}}">
+                       <input class="sh_hot_shops" name="filter" type="checkbox">
+                       <label>{{$value}}</label>
                    </span>
             @endforeach
             </div>
@@ -49,6 +49,140 @@
             </tr>
         </table>
 </div>
+
+<script type="text/template" id="shop_list_template">
+
+<% for(var n = 0; n < Math.ceil(shops.length / 5); n++) { %>
+    <div class="more_shops-row">
+        <%  for (var i = n * 5 ; (i < n * 5 + 5) && (i < shops.length); i++){ %>
+            <a href="<%= shops[i]['shop_url'] %>" class="restaurant-<%= shops[i]['shop_id'] %>">
+            
+             <% if(shops[i]['is_opening']){ %>
+                  <div class="more_shops-row-book more_shops-row-colse" data-support_activity="<%= shops[i]['support_activity'].join(',') %>" data-isHot="<%= Number(shops[i]['isHot']) %>" data-isOnline="<%= Number(shops[i]['isOnline']) %>" data-isSupportPay="<%= Number(shops[i]['isSupportPay']) %>" data-flavor="<%=  shops[i]['flavor'] ? shops[i]['flavor'] : ''  %>" data-shop_id="<%= shops[i]['shop_id'] %>" data-place_id="<%= shops[i]['place_id'] %>">
+            <% } else { %>
+                  <div class="more_shops-row-book" data-support_activity="<%= shops[i]['support_activity'].join(',') %>" data-isHot="<%= Number(shops[i]['isHot']) %>" data-isOnline="<%= Number(shops[i]['isOnline']) %>" data-isSupportPay="<%= shops[i]['isSupportPay'] %>" data-flavor="<% shops[i]['flavor'] ? shops[i]['flavor'] : ''  %>" data-shop_id="<%= shops[i]['shop_id'] %>" data-place_id="<%= shops[i]['place_id'] %>">
+            <% } %>
+
+
+<% if(shops[i]['is_collected']){ %>
+                    <div class="collect_star">
+
+                    </div>
+                    <div class="collect">
+                        收藏
+                    </div>
+                    <div class="uncollect change">
+                        取消收藏
+                    </div>
+            <% } else { %>
+                    <div class="collect_star" style="display: none">
+
+                    </div>
+                    <div class="collect change">
+                        收藏
+                    </div>
+                    <div class="uncollect">
+                        取消收藏
+                    </div>
+            <% } %>
+                    <div class="more_shops-row-book-left">
+                        <div class="logo">
+
+            <% if(shops[i]['shop_logo']){ %>
+                                    <img src="<%= shops[i]['shop_logo'] %>"/>
+            <% } else { %>
+                                    <img src='images/eleme_restaurant_logo.jpg'/>
+            <% } %>
+
+                        </div>
+            <% if (!shops[i]['deliver_time']){ %>
+                                <span title="该餐厅刚开张，暂无送餐时间数据"></span>
+            <% } else if (shops[i]['deliver_time'] < 45){ %>
+                                <span title="平均送餐时间<%= shops[i]['deliver_time'] %>分钟"><%= shops[i]['deliver_time'] %>分钟</span>
+            <% } else { %>
+                                <span class="long_time" title="当前餐厅送餐较慢">45+分钟</span>
+            <% } %>
+                    </div>
+                    <div class="more_shops-row-book-right">
+                        <div class="title">
+                            <p><%= shops[i]['shop_name'] %></p>
+                        </div>
+            <% if(shops[i]['is_opening']){ %>
+                        <div class="busy">
+                <% if(shops[i]['is_opening'] == 1 ){ %>
+                            <span>休息中</span>
+                        </div>
+                        <div class="time">
+                            <span title="休息中，暂不接受订单">休息中暂不接受订单</span>
+                        </div>
+                <% } else{ %>
+                            <span>太忙了</span>
+                        </div>
+                        <div class="time">
+                            <span title="太忙了，暂不接受订单">太忙了暂不接受订单</span>
+                        </div>
+                <% } %>
+            <%  } else { %>
+                <% if(shops[i]['is_ready_for_order']){ %>
+                            <div class="reserve">
+                                <span>接受预定</span>
+                            </div>
+                            <div class="time">
+                                <span title="接受预定，送餐时间<%= shops[i]['deliver_start'] %>开始">送餐时间<%= shops[i]['deliver_start'] %>开始</span>
+                            </div>
+                <% } %>
+                            <div class="style">
+                            <span><%= shops[i]['shop_type'] %></span>
+                        </div>
+                        <div class="remark">
+                            <div class="star" title="餐厅评价：<%= shops[i]['shop_level'] %>星" style="background-position: -1px <%= -334+15.5*Math.ceil(shops[i]['shop_level']*2) %>px"></div>
+                <% if(shops[i]['order_count']){ %>
+                                        <span><%= shops[i]['order_count'] %>订单</span>
+                <% } %>
+                            </div>
+                        <div class="icon">
+                <% for(var m = 0; m < shops[i]['support_activity'].length ; m++) { %>
+                                        <span title="<%= shop_activity[shops[i]['support_activity'][m]] %>" style="background-position: 0 <%= 20-shops[i]['support_activity'][m]*20 %>px %>" ></span>
+                <% } %>
+                        </div>
+            <% } %>
+
+                    </div>
+
+                    <div class="more_info more_info_right">
+                        <div class="tip"></div>
+                        <div style="font-size:15px; margin-left:10px;margin-bottom: 10px;">
+                            <%= shops[i]['shop_name'] %>
+                        </div>
+            <% if(shops[i]['is_opening']==1){ %>
+                                <p class="close">已打烊，<%= shops[i]['deliver_start'] %>开始订餐</p>
+            <% } else if(shops[i]['is_opening']==2){ %>
+                                <p class="close">餐厅太忙，暂不接受订单</p>
+            <% } %>
+                        <ul>
+            <% for(var m = 0; m < shops[i]['support_activity'].length; m++){ %>
+                                    <li>
+                                        <span class="icon" style="background-position: 0 <%= 20-shops[i]['support_activity'][m]*20 %>px"></span>
+                                        <span><%= shop_activity[shops[i]['support_activity'][m]] %> </span>
+                                    </li>
+            <% } %> 
+                        </ul>
+                        <div class="divider">
+                            <p class="ann"><strong>公告:</strong>&nbsp;<%= shops[i]['shop_announce'] %></p>
+                            <p><strong>起送价:</strong>&nbsp;<%= shops[i]['deliver_state_start'] %></p>
+                            <p><strong>地址:</strong>&nbsp;<%= shops[i]['shop_address'] %></p>
+                            <p><strong>营业时间:</strong>&nbsp;<%= shops[i]['business_hours'] %></p>
+                            <p><strong>简介:</strong>&nbsp;<%= shops[i]['shop_summary'] %></p>
+                        </div>
+                    </div>
+                </div>
+            </a>    
+
+        <% } %>
+    </div>
+<% } %>
+
+</script>
 
 @section("css")
     @parent
--- public/js/widget/shop_list/shop_list.js
+++ public/js/widget/shop_list/shop_list.js
@@ -1,39 +1,55 @@
 define([ "jquery", "tools/Sizer" ], function($, Sizer) {
     // 事件触发的数据获取中间件
     function dataTrigger(ev, type) {
-        var obj = {};
-        if ("checkbox" == type) {
+        var spans, activities, obj = {};
+        if ("checkbox" == type || "activity" == type) {
             var deleteTarget = ev.delegateTarget, input = $(deleteTarget).find("input");
             if (input) {
                 var nowChecked = input[0].checked;
                 input[0].checked = !nowChecked;
             }
-        } else {
+        } else if ("drop" == type) {
             var target = ev.target;
-            obj.flavor = $(target).html();
+            $(".drop_button").find("a").html(target), obj.flavor = $(target).html();
         }
-        var spans = $(".choice_click");
-        spans.each(function() {
+        "checkbox" == type || "drop" == type ? (spans = $(".choice_click"), spans.each(function() {
             var input = $(this).find("input"), text = $(this).find("label"), checked = input[0].checked, label = (text.html(), 
             $(this).data("label"));
             checked && (obj[label] = Number(checked));
-        }), console.log(Sizer.get(obj));
+        })) : (spans = $(".activities-btn"), activities = [], spans.each(function() {
+            var input = $(this).find("input"), activity = $(this).data("activity_id"), checked = input[0].checked;
+            checked && activities.push(activity);
+        }), obj.support_activity = activities), console.log(Sizer.get(obj));
+        var template = _.template($("#shop_list_template").html())({
+            shops: Sizer.get(obj),
+            shop_activity: shop_activity
+        });
+        shop_container.find("td").html(template);
     }
-    var drop_button = $(".drop_button"), drop_list = $(".drop_list"), choice_click = ($(".activities-btn"), 
-    $(".shops_func"), $(".choice_click"));
-    drop_button.on("click", function() {
+    var drop_button = $(".drop_button"), drop_list = $(".drop_list"), activitiesBtn = $(".activities-btn"), choice_click = ($(".shops_func"), 
+    $(".choice_click")), shop_activity = [], shop_container = $(".shop_container");
+    activitiesBtn.each(function() {
+        shop_activity.push({
+            id: $(this).data("activity_id"),
+            value: $(this).find("label").html()
+        });
+    }), drop_button.on("click", function() {
         return drop_list.toggle(), $(this).toggleClass("active"), !1;
     }), function() {
-        var target = $(".shop_container .more_shops-row-book"), result = [];
+        var target = shop_container.find(".more_shops-row-book"), result = [];
         target.each(function() {
             var target = $(this), place_id = target.data("place_id"), shop_id = target.data("shop_id"), flavor = target.data("flavor"), issupportpay = target.data("issupportpay"), isonline = target.data("isonline"), ishot = target.data("ishot"), support_activity = target.data("support_activity").split(","), storage = {};
             storage.place_id = place_id, storage.shop_id = shop_id, storage.flavor = flavor, 
             storage.issupportpay = issupportpay, storage.isonline = isonline, storage.ishot = ishot, 
             storage.support_activity = support_activity, storage.shop_id = shop_id, result.push(storage);
         }), console.log(result), Sizer.add(result);
-    }(), drop_list.on("click", "li", function(ev) {
-        console.log(ev.target);
-    }), choice_click.on("click", dataTrigger), drop_list.on("click", "li", function(ev) {
+    }(), drop_list.on("click", "li", function(e) {
+        dataTrigger(e, "drop");
+    }), choice_click.on("click", function(e) {
+        dataTrigger(e, "checkbox");
+    }), activitiesBtn.on("click", function(e) {
+        dataTrigger(e, "activity");
+    }), drop_list.on("click", "li", function(ev) {
         {
             var target = ev.currentTarget;
             target.innerHTML;
