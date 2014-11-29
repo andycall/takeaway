01c5e58689d071bed6ca83460b5dcc4854d33f09
Liuchenling <wssgcg1213@qq.com>
2014-11-29 15:05:46 +0800

    add comment for cart in port

--- app/views/template/shop/port.js
+++ app/views/template/shop/port.js
@@ -4,10 +4,10 @@
 
 define(function(){
 	return {
-		cartSetCount : "./cartSetCount",
-        cartClear: "./cartClear",
-        cartAdd: "./cartAdd",
-        cartDel: "./cartDel",
+		cartSetCount : "./cartSetCount",  //购物车设置商品数量
+        cartClear: "./cartClear",   //购物车清空
+        cartAdd: "./cartAdd",       //购物车根据 id 添加商品
+        cartDel: "./cartDel",       //购物车根据 id 删除商品
 
 	}
 
--- public/js/template/shop/port.js
+++ public/js/template/shop/port.js
@@ -4,8 +4,11 @@
 define(function() {
     return {
         cartSetCount: "./cartSetCount",
+        //购物车设置商品数量
         cartClear: "./cartClear",
+        //购物车清空
         cartAdd: "./cartAdd",
+        //购物车根据 id 添加商品
         cartDel: "./cartDel"
     };
 });
